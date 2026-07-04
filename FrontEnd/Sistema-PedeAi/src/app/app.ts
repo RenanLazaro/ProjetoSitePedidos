13df4c3d30d6844ecb4daecb04c906a47b91479e
Kauan1104 <86495141+Kauan1104@users.noreply.github.com>
2026-07-04 16:04:46 -0300

    mudança de nome no front para PedeAi - arquivos alterados.

--- FrontEnd/Sistema-PedeAi/src/app/app.ts
+++ FrontEnd/Sistema-PedeAi/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('sistema-de-pedido');
+  protected readonly title = signal('PedeAi');
 }
